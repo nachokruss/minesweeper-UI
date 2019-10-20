f9e3f58c65b172e536afbee5fc6434e4633fd4aa
Ignacio Kruss <ignacio.kruss@adroll.com>
2019-10-20 17:41:51 -0300

    refactoring width to rows and height to cols and added fetch functionality

--- minesweeper-ui/src/components/App.js
+++ minesweeper-ui/src/components/App.js
@@ -7,28 +7,41 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      width: 9,
-      height: 9,
+      rows: 9,
+      cols: 9,
       mines: 3,
-      game: {}
+      game: {},
+      gameId: ''
     };
     this.onNewClick = this.onNewClick.bind(this);
     this.onCellClick = this.onCellClick.bind(this);
     this.onCellFlag = this.onCellFlag.bind(this);
+    this.onFetchClick = this.onFetchClick.bind(this);
   }
 
   render() {
-    const inputStyle = {
+    const smallInputStyle = {
       width: '30px',
+      margin: '10px',
+    };
+    const largeInputStyle = {
+      width: '160px',
       margin: '10px',
     };
     return <div>
       <h1>Minesweeper</h1>
       <div>
-        Rows:<input style={inputStyle} onChange={(e) => {this.setState({width: parseInt(e.target.value)})}} value={this.state.width} />
-        Columns:<input style={inputStyle} onChange={(e) => {this.setState({height: parseInt(e.target.value)})}} value={this.state.height} />
-        Mines:<input style={inputStyle} onChange={(e) => {this.setState({mines: parseInt(e.target.value)})}} value={this.state.mines} />
+        Rows:<input style={smallInputStyle} onChange={(e) => {this.setState({rows: parseInt(e.target.value)})}} value={this.state.rows} />
+        Columns:<input style={smallInputStyle} onChange={(e) => {this.setState({cols: parseInt(e.target.value)})}} value={this.state.cols} />
+        Mines:<input style={smallInputStyle} onChange={(e) => {this.setState({mines: parseInt(e.target.value)})}} value={this.state.mines} />
         <button onClick={this.onNewClick}>New</button>
+      </div>
+      <div>
+        Game ID:<input style={largeInputStyle} onChange={(e) => {this.setState({gameId: e.target.value})}} value={this.state.gameId} />
+        <button onClick={this.onFetchClick}>Fetch</button>
+      </div>
+      <div>
+        <h4>Use left click to reveal and right click to flag</h4>
       </div>
       <Minesweeper
           game={this.state.game}
@@ -40,9 +53,10 @@ class App extends React.Component {
 
   onNewClick() {
     const state = this.state;
-    MinesweeperApi.newGame(state.width, state.height, state.mines)
+    MinesweeperApi.newGame(state.rows, state.cols, state.mines)
         .then(data => this.setState({
-          game: data
+          game: data,
+          gameId: data.id,
         }));
   }
 
@@ -55,6 +69,13 @@ class App extends React.Component {
 
   onCellFlag(x, y) {
     MinesweeperApi.flag(this.state.game.id, x, y)
+        .then(data => this.setState({
+          game: data
+        }));
+  }
+
+  onFetchClick() {
+    MinesweeperApi.fetchGame(this.state.gameId)
         .then(data => this.setState({
           game: data
         }));
